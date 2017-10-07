a0343a7d3f0211bcd7b570ff8efaf03bb56cd920
Wouter Saaltink <wouter.saaltink@gmail.com>
2017-10-07 12:24:55 +0200

    first working version

--- program.cpp
+++ program.cpp
@@ -1,212 +1,79 @@
 #include <iostream>
 #include <sstream>
-#include <winsock2.h>
-#include <windows.h>
-#include <ws2tcpip.h>
+#include <string>
 #include <map>
-#include <algorithm>
+#include <direct.h>
 
-#define DEFAULT_BUFLEN 512
-#define DEFAULT_PORT "443"
+#include <system.io.fileinfo.h>
+#include <system.io.path.h>
 
-// trim from start
-static inline std::string &ltrim(std::string &s) {
-    s.erase(s.begin(), std::find_if(s.begin(), s.end(),
-            std::not1(std::ptr_fun<int, int>(std::isspace))));
-    return s;
-}
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
 
-// trim from end
-static inline std::string &rtrim(std::string &s) {
-    s.erase(std::find_if(s.rbegin(), s.rend(),
-            std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
-    return s;
-}
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
+#include <stb_image_resize.h>
 
-// trim from both ends
-static inline std::string &trim(std::string &s) {
-    return ltrim(rtrim(s));
-}
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb_image_write.h>
 
-void parseResponse(std::string& allData)
-{
-    std::string _method;
-    std::string _uri;
-    std::map<std::string, std::string> _headers;
-    std::string _payload;
-
-    auto pos = allData.find("\r\n\r\n");
-    if (pos != std::string::npos)
-    {
-        std::stringstream ss;
-        ss.str(allData.substr(0, pos));
-        std::string line;
-
-        // Determine methode, uri and HTTP version
-        if (std::getline(ss, line))
-        {
-            trim(line);
-            std::cout << line << std::endl;
-            auto first = line.find_first_of(' ');
-            auto second = line.find_first_of(' ', first + 1);
-            if (first != std::string::npos && second != std::string::npos)
-            {
-                auto version = line.substr(second);
-//                if (trim(version) != "HTTP/1.1") throw new std::runtime_error("invalid HTTP version");
-
-                auto method = line.substr(0, first);
-                _method = trim(method);
-
-                auto uri = line.substr(first, second-first);
-                _uri = trim(uri);
-            }
-        }
-
-        // Determine headers
-        while (std::getline(ss, line))
-        {
-            auto pos = line.find_first_of(':');
-            if (pos != std::string::npos)
-            {
-                auto key = line.substr(0, pos);
-                auto value = line.substr(pos + 1);
-                _headers.insert(std::make_pair(trim(key), trim(value)));
-            }
-        }
-
-        // And finally determine payload(if any)
-        _payload = allData.substr(pos + 4);
-    }
-
-    for (auto pair : _headers)
-    {
-        std::cout << pair.first << ":" << pair.second << std::endl;
-    }
-
-    std::cout << _payload << std::endl;
-}
+using namespace System::IO;
 
 int main(int argc, char* argv[])
 {
-    WSADATA wsaData;
-    SOCKET ConnectSocket = INVALID_SOCKET;
-    struct addrinfo *result = NULL, *ptr = NULL, hints;
-    std::string data = "entry.1595413962=Naam&entry.635739924=Data";
+    std::map<std::string, int> drawable_sizes = {
+        std::make_pair("drawable-hdpi", 72),
+        std::make_pair("drawable-mdpi", 48),
+        std::make_pair("drawable-xhdpi", 96),
+        std::make_pair("drawable-xxhdpi", 144)
+    };
 
-    // http://docs.google.com/forms/d/e/1FAIpQLSftLcKlBFTAodyDKI8kzZcDcc6uBUbsfp4cpQLiGbFuFT58_Q/formResponse
-    std::stringstream ss;
-    ss << "POST /forms/d/e/1FAIpQLSftLcKlBFTAodyDKI8kzZcDcc6uBUbsfp4cpQLiGbFuFT58_Q/formResponse HTTP/1.1\r\n"
-       << "Host: docs.google.com\r\n"
-       << "Content-type: application/x-www-form-urlencoded; charset=utf-8\r\n"
-       << "Content-length: " << data.size() << "\r\n"
-       << "Connection: close\r\n"
-       << "\r\n"
-       << data;
-    char recvbuf[DEFAULT_BUFLEN + 1];
-    int iResult;
-    int recvbuflen = DEFAULT_BUFLEN;
-
-    // Initialize Winsock
-    iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
-    if (iResult != 0)
+    if (argc < 2)
     {
-        printf("WSAStartup failed with error: %d\n", iResult);
-        return 1;
+        std::cout << "ERROR: too few arguments, I need an input image filename\n";
+        return -1;
     }
 
-    ZeroMemory( &hints, sizeof(hints) );
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_protocol = IPPROTO_TCP;
+    auto file = FileInfo(argv[1]);
 
-    // Resolve the server address and port
-    iResult = getaddrinfo("docs.google.com", DEFAULT_PORT, &hints, &result);
-    if ( iResult != 0 )
+    if (!file.Exists())
     {
-        printf("getaddrinfo failed with error: %d\n", iResult);
-        WSACleanup();
-        return 1;
+        std::cout << "ERROR: given file does not exist: " << file.FullName() << "\n";
+        return -2;
     }
 
-    // Attempt to connect to an address until one succeeds
-    for(ptr=result; ptr != NULL ;ptr=ptr->ai_next)
+    auto directory = file.Directory();
+
+    int x, y, channels;
+    auto pixels = stbi_load(file.FullName().c_str(), &x, &y, &channels, 4);
+
+    if (pixels == nullptr)
     {
-        // Create a SOCKET for connecting to server
-        ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
-        if (ConnectSocket == INVALID_SOCKET)
+        std::cout << "ERROR: could not load image from given filename: " << file.FullName() << "\n";
+        return -2;
+    }
+
+    for (std::pair<std::string, int> p : drawable_sizes)
+    {
+        auto drawableDir = DirectoryInfo(Path::Combine(directory.FullName(), p.first));
+        if (!drawableDir.Exists())
         {
-            printf("socket failed with error: %ld\n", WSAGetLastError());
-            WSACleanup();
-            return 1;
+            mkdir(drawableDir.FullName().c_str());
         }
 
-        // Connect to server.
-        iResult = connect( ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
-        if (iResult == SOCKET_ERROR)
+        stbi_uc* outputpixels = new stbi_uc[p.second * p.second * channels];
+        if (stbir_resize_uint8(pixels, x, y, 0, outputpixels, p.second, p.second, 0, channels))
         {
-            closesocket(ConnectSocket);
-            ConnectSocket = INVALID_SOCKET;
-            continue;
+            auto ic_launcher = FileInfo(Path::Combine(drawableDir.FullName(), "ic_launcher.png"));
+            if (stbi_write_png(ic_launcher.FullName().c_str(), p.second, p.second, channels, outputpixels, 0))
+            {
+                std::cout << "Successfully written " << p.first << " with size " << p.second << "x" << p.second << " to " << drawableDir.FullName() << "\n";
+            }
+            else
+            {
+                std::cout << "Failed to write " << p.first << " with size " << p.second << "x" << p.second << " to " << drawableDir.FullName() << "\n";
+            }
         }
-
-        setsockopt(ConnectSocket, SOL_SOCKET, SO_SECURE, (DWORD)SO_SEC_SSL, 0);
-
-        break;
     }
-
-    freeaddrinfo(result);
-
-    if (ConnectSocket == INVALID_SOCKET) {
-        printf("Unable to connect to server!\n");
-        WSACleanup();
-        return 1;
-    }
-
-    // Send an initial buffer
-    iResult = send( ConnectSocket, ss.str().c_str(), (int)ss.str().size(), 0 );
-    if (iResult == SOCKET_ERROR)
-    {
-        printf("send failed with error: %d\n", WSAGetLastError());
-        closesocket(ConnectSocket);
-        WSACleanup();
-        return 1;
-    }
-
-    printf("Bytes Sent: %ld\n", iResult);
-
-    // shutdown the connection since no more data will be sent
-    iResult = shutdown(ConnectSocket, SD_SEND);
-    if (iResult == SOCKET_ERROR)
-    {
-        printf("shutdown failed with error: %d\n", WSAGetLastError());
-        closesocket(ConnectSocket);
-        WSACleanup();
-        return 1;
-    }
-
-    std::string response;
-    // Receive until the peer closes the connection
-    do {
-
-        iResult = recv(ConnectSocket, recvbuf, recvbuflen, 0);
-        if ( iResult > 0 )
-        {
-            recvbuf[iResult] = '\0';
-            printf("Bytes received: %d\n", iResult);
-            response += recvbuf;
-        }
-        else if ( iResult == 0 )
-            printf("Connection closed\n");
-        else
-            printf("recv failed with error: %d\n", WSAGetLastError());
-
-    } while( iResult > 0 );
-
-    parseResponse(response);
-
-    // cleanup
-    closesocket(ConnectSocket);
-    WSACleanup();
 
     return 0;
 }
